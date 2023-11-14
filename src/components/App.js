197d1e120d8aeae7710d0594c38cadc1df582f63
Karuga Mbugua <karugambugua@icloud.com>
2023-11-14 13:28:11 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,14 +8,30 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
+    const [tasks, setTasks] = useState(TASKS);
+    const [selectedCategory, setSelectedCategory] = useState(CATEGORIES[0]);
+
+    const deleteTask = (taskId) => {
+        const updatedTasks = tasks.filter(task => task.id !== taskId);
+        setTasks(updatedTasks);
+    }
+
+    const handleFilterChange = (category) => {
+        setSelectedCategory(category);
+    }
+
+    return (
+        <div className="App">
+            <h2>My tasks</h2>
+            <CategoryFilter 
+                categories={CATEGORIES} 
+                selectedCategory={selectedCategory} 
+                onFilterChange={handleFilterChange} 
+            />
+            <NewTaskForm />
+            <TaskList tasks={tasks} deleteTask={deleteTask}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Task() {
-  return (
+function Task({ task, deleteTask }) {
+  
+ return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.name}</div>
+      <button className="delete" onClick={() => deleteTask(task.id)}>X</button>
     </div>
-  );
+ );
 }
 
 export default Task;
